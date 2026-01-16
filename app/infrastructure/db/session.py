f6aaabbfa47fe149665f396d315676e990300a6c
SumburovSN <44998227+SumburovSN@users.noreply.github.com>
2026-01-16 21:57:11 +0400

    Update session.py

--- app/infrastructure/db/session.py
+++ app/infrastructure/db/session.py
@@ -14,14 +14,3 @@ def create_session_maker(database_url: str) -> async_sessionmaker[AsyncSession]:
         engine,
         expire_on_commit=False,
     )
-
-
-
-# from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-#
-# def create_session_maker(database_url: str) -> async_sessionmaker[AsyncSession]:
-#     engine = create_async_engine(
-#         database_url,
-#         pool_pre_ping=True,
-#     )
-#     return async_sessionmaker(engine, expire_on_commit=False)
