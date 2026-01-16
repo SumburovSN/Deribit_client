30834e1c60e7c7b3d13953afd31e0aa4c85d9a96
SumburovSN <44998227+SumburovSN@users.noreply.github.com>
2026-01-16 21:57:47 +0400

    Update base.py

--- app/infrastructure/db/repositories/base.py
+++ app/infrastructure/db/repositories/base.py
@@ -28,31 +28,3 @@ class PriceReaderRepository(ABC):
         limit: int = 100,
     ) -> list[Price]:
         pass
-
-
-# from abc import ABC, abstractmethod
-# from app.domain.models import Price
-#
-#
-# class PriceRepository(ABC):
-#
-#     @abstractmethod
-#     async def add(self, price: Price) -> None:
-#         pass
-#
-#     @abstractmethod
-#     async def get_all_by_ticker(self, ticker: str) -> list[Price]:
-#         pass
-#
-#     @abstractmethod
-#     async def get_last_by_ticker(self, ticker: str) -> Price | None:
-#         pass
-#
-#     @abstractmethod
-#     async def get_by_ticker_and_period(
-#         self,
-#         ticker: str,
-#         from_ts: int,
-#         to_ts: int,
-#     ) -> list[Price]:
-#         pass
